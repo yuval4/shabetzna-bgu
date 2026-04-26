6c7d7e859a60c661dba81ace58a8226bd733f1e4
pictor360 <notpictor@gmail.com>
2026-04-26 21:58:10 +0300

    small change

--- shabetzna-client/src/main.tsx
+++ shabetzna-client/src/main.tsx
@@ -1,8 +1,8 @@
 import posthog from 'posthog-js';
 import { PostHogProvider } from 'posthog-js/react';
 import React from "react";
-import ReactDOM from "react-dom/client";
 import App from "./app";
+import ReactDOM from "react-dom/client";
 
 if (!posthog.__loaded) posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
   api_host: import.meta.env.VITE_POSTHOG_HOST,
